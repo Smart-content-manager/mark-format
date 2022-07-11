4efb93d2bd6834259434d2c0bb2016112b9878cc
Death <ricardo.pajaro.bc2d@gmail.com>
2022-07-11 15:12:04 -0500

    add selector time 008/00-05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ControlFieldsModule} from "./control-fields/control-fields.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ControlFieldsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
